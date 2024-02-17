bf0720c6cb921c840566d5f9d2570358fc9542ac
YAIR-N <ynotkovch@gmail.com>
2024-02-17 22:56:51 +0200

    fixed get user

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -23,8 +23,8 @@ public class UsersController : ControllerBase
     }
 
     [HttpGet("{id}")]
-    public async Task<ActionResult<AppUser>> GetUsers(int id)
+    public async Task<ActionResult<AppUser>> GetUser(int id)
     {
-        return await _context.Users.FindAsync();
+        return await _context.Users.FindAsync(id);
     }
 }
